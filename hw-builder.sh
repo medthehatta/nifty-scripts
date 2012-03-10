df99caff19e88fb2bee422b8d424d1a57834c957
Med <medthehatta@gmail.com>
2012-03-09 22:27:02 -0500

    Lots of changes to hw-builder.sh

--- hw-builder.sh
+++ hw-builder.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+#
+# Bash script for producing a nicely LaTeXed file from RST
+#
+
+
+# -- Various Settings -- #
+
+PDFPREFIX="med-"
 BASE="/home/med/class/grad/tex"
 PREAMBLE="$BASE/preamble.tex"
 RSTCFG="$BASE/rst.conf"
-RST="/usr/bin/rst2latex.py"
+RST=`which rst2latex`
+TEX=`which pdflatex`
+
+# -- End of Settings -- #
+
 
 TMPCONF=`mktemp`
 
@@ -10,36 +23,31 @@ ASSIGN=`basename ${1} | cut -d'.' -f1`
 cp -r "$RSTCFG" "$TMPCONF"
 echo "latex-preamble:" >> "$TMPCONF"
 sed 's/^/\t/g' "$PREAMBLE" >> "$TMPCONF"
-CMD="python2 $RST --conf=$TMPCONF $ASSIGN.rst"
+CMD="$RST --conf=$TMPCONF $ASSIGN.rst"
 DEST=$ASSIGN.tex
 echo $CMD
 $CMD > "$DEST"
 
-pdflatex "$ASSIGN.tex" 
+"$TEX" "$ASSIGN.tex" 
 if [ $? -eq 0 ]; then
 	echo && echo && echo
 
-	if [ -e "$ASSIGN.sage" ]; then
-		echo "$ASSIGN.sage Found in $(pwd)."
-		sage "$ASSIGN.sage"
-	fi
-
-	if [ -n $(ls *.bib) ]; then
-		echo "Bibfile found in $(pwd)."
+	if [ -e "$(ls *.bib)" ]; then
+		echo "Processing citations"
 		bibtex "$ASSIGN"
 		echo && echo && echo
-		pdflatex "$ASSIGN.tex"
+		"$TEX" "$ASSIGN.tex"
 	fi
 
-	pdflatex "$ASSIGN.tex" && mv "$ASSIGN.pdf" "med-${ASSIGN}.pdf"
+	"$TEX" "$ASSIGN.tex" && mv "$ASSIGN.pdf" "${PDFPREFIX}${ASSIGN}.pdf"
 
 	if [ $? -eq 0 ]; then
 		rm -f "$TMPCONF"
 		rm -f *.{out,log,aux}
 		rm -f *.{blg,bbl}
-		#rm -f "$ASSIGN".{sage,py,sout}
-		rm -rf sage-plots-for*
 		rm -f "$ASSIGN.tex"
 	fi
+else
+	return -1
 fi
 
