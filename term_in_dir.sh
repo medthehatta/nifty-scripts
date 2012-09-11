32e3cb6f5744913390af0e1cf0c856d03e28aea6
Med Mahmoud <med@temple.edu>
2012-09-11 18:23:21 -0400

    Added term_in_dir.sh for opening a screen session in a given dir

---
+++ term_in_dir.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# If the first arg is a dir, use that dir
+#  and assume the name is the dir base
+# If the first arg is NOT a dir, use $HOME
+#  and assume the name is the first arg
+if [ -d "$1" ]; then
+  WHERE="$1"
+  NAME=$(basename "$1")
+else
+  WHERE="$HOME"
+  NAME="$1"
+fi
+
+cd "$WHERE"
+
+# Read in the second argument
+# If it exists, it's specifying the name
+shift
+if (( "$#" )); then
+  NAME="$1"
+fi
+
+# Launch screen in the directory with the given name
+urxvt -e screen -U -d -R -S "$NAME" &
+
