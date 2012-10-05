4160d54a2f7e147caa3500bde1c6df1ef3e357d8
Med Mahmoud <med@temple.edu>
2012-10-05 09:57:44 -0400

    Added catnotes.sh

---
+++ catnotes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+P="$@"
+NUM=5
+TMP=$(mktemp)
+PAGER="less"
+CAT="cat"
+
+FILES=$(find "$P" -type f | grep -iv swp | sort -r | head -n${NUM} | sort)
+$CAT ${FILES} > "${TMP}"
+
+$PAGER "${TMP}"
+
+
